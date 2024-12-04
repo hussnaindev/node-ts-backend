5b0fdf5a79e7598f0a89fb8e0480e786656aa419
Hussnain Raza <hussnain444all@gmail.com>
2024-12-04 16:31:56 +0500

    added logging and try catch handling in auth service file

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -1,38 +1,70 @@
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
+import { v4 as uuid } from 'uuid';
+import { LOG_GROUPS, LOG_STREAMS } from '../constants/constants';
 import User from '../models/sequelize/UserModel';
-
+import Logger from '../utils/Logger';
+import { tryCatch } from '../utils/decorators/tryCatch';
 export class AuthService {
+        private logger: Logger;
+
+        constructor() {
+                const reqId = uuid();
+                this.logger = new Logger(LOG_GROUPS.NODE_SERVER_LOGS, LOG_STREAMS.REQUEST_LOGS, reqId);
+        }
+
+        @tryCatch('Failed to hash password')
         async hashPassword(password: string): Promise<string> {
+                this.logger.info('Hashing password...')
                 const saltRounds = 10;
-                return await bcrypt.hash(password, saltRounds);
+                const hashedPass = await bcrypt.hash(password, saltRounds)
+                this.logger.info('Password hashed successfully!')
+                return hashedPass;
         }
 
+        @tryCatch('Failed to verify the password')
         async verifyPassword(password: string, hashedPassword: string): Promise<boolean> {
-                return await bcrypt.compare(password, hashedPassword);
+                this.logger.info('verifying password...')
+                const res = await bcrypt.compare(password, hashedPassword);
+                this.logger.info('Password verification process completed!')
+                return res;
         }
 
+        @tryCatch('Failed to generate the token')
         generateToken(user: User): string {
+                this.logger.info(`Generating token...`)
                 const payload = {
                         id: user.id,
                         username: user.name,
                         email: user.email,
                         roles: user.userType,
                 };
-                return jwt.sign(payload, process.env.JWT_SECRET || '', { expiresIn: '1h' });
+                const res = jwt.sign(payload, process.env.JWT_SECRET || '', { expiresIn: '1h' });
+                this.logger.info(`Token generated successfully`);
+                return res;
         }
 
+        @tryCatch('Failed to reset the password')
         async resetPassword(user: User, newPassword: string) {
+                this.logger.info(`Resetting password...`)
                 const hashedPassword = await this.hashPassword(newPassword);
                 await user.update({ password: hashedPassword });
+                this.logger.info(`Password reset successfully!`)
         }
 
+        @tryCatch('Failed to change the password')
         async changePassword(user: User, newPassword: string) {
+                this.logger.info(`Changing password...`)
                 const hashedPassword = await this.hashPassword(newPassword);
                 await user.update({ password: hashedPassword });
+                this.logger.info(`Password changed successfully!`)
         }
 
+        @tryCatch('Failed to fetch user password')
         async getUserPassword(userId: number) {
-                return (await User.findByPk(userId, { attributes: ['password'] }))?.password;
+                this.logger.info(`Fetching user password...`);
+                const res = (await User.findByPk(userId, { attributes: ['password'] }))?.password;
+                this.logger.info(`User password fetched successfully!`);
+                return res;
         }
 }
