4802319336d1663a53c27eebefe12538fc0e7bba
Hussnain Raza <hussnain444all@gmail.com>
2024-12-05 15:18:41 +0500

    added logging in S3

--- src/integrations/aws/s3/S3.ts
+++ src/integrations/aws/s3/S3.ts
@@ -1,19 +1,24 @@
 import { DeleteObjectCommand, GetObjectCommand, ListObjectsCommand, PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
+import { LOG_GROUPS, LOG_STREAMS } from '../../../constants/constants';
 import { tryCatch } from '../../../utils/decorators/tryCatch';
+import Logger from '../../../utils/Logger';
 
 
 class S3 {
     private s3Client: S3Client;
     private bucketName: string;
+    private logger: Logger;
 
-    constructor(bucketName?: string) {
+    constructor(bucketName?: string, reqId?: string) {
         this.s3Client = new S3Client();
         this.bucketName = bucketName || process.env.AWS_S3_BUCKET_NAME || '';
+        this.logger = new Logger(LOG_GROUPS.NODE_SERVER_LOGS, LOG_STREAMS.REQUEST_LOGS, reqId);
     }
 
     @tryCatch('Failed to upload object to S3')
     public async uploadObject(key: string, body: Buffer | string, contentType?: string): Promise<void> {
+        this.logger.info('Uploading object to S3...');
         const command = new PutObjectCommand({
             Bucket: this.bucketName,
             Key: key,
@@ -21,10 +26,12 @@ class S3 {
             ContentType: contentType,
         });
         await this.s3Client.send(command);
+        this.logger.info('Object uploaded to S3 successfully!');
     }
 
     @tryCatch('Failed to retrieve object from S3')
     public async getObject(key: string): Promise<Buffer | undefined> {
+        this.logger.info('Fetching object from S3...');
         const command = new GetObjectCommand({
             Bucket: this.bucketName,
             Key: key,
@@ -37,34 +44,42 @@ class S3 {
             for await (const chunk of response.Body as any) {
                 chunks.push(chunk);
             }
-            return Buffer.concat(chunks);
+            const res = Buffer.concat(chunks);
+            this.logger.info(`${res.length} Objects fetched from S3 successfully!`);
+            return res
         }
 
+        this.logger.info('No object found from S3!');
         return undefined;
     }
 
     @tryCatch('Failed to delete object from S3')
     public async deleteObject(key: string): Promise<void> {
+        this.logger.info('Deleting object from S3...');
         const command = new DeleteObjectCommand({
             Bucket: this.bucketName,
             Key: key,
         });
         await this.s3Client.send(command);
+        this.logger.info('Object deleted from S3 successfully!');
     }
 
     @tryCatch('Failed to list objects in S3 bucket')
     public async listObjects(prefix?: string): Promise<string[]> {
+        this.logger.info('Fetching objects from S3...');
         const command = new ListObjectsCommand({
             Bucket: this.bucketName,
             Prefix: prefix,
         });
 
         const response = await this.s3Client.send(command);
+        this.logger.info('Objects fetched from S3 successfully!');
         return response.Contents?.map((item) => item.Key || '') || [];
     }
 
     @tryCatch('Failed to get image URLs')
     public async getImageUrls(prefix?: string): Promise<{ key: string; url: string }[]> {
+        this.logger.info('Fetching images urls from S3...');
         const keys = await this.listObjects(prefix);
 
         const signedUrlPromises = keys.map(async (key) => {
@@ -76,7 +91,9 @@ class S3 {
             return { key, url };
         });
 
-        return Promise.all(signedUrlPromises);
+        const imagesUrls = await Promise.all(signedUrlPromises);
+        this.logger.info('Images urls fetched from S3 successfully!');
+        return imagesUrls;
     }
 }
 
